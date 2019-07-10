5c06913337b856a5596380957d18096fe73f4722
success <nkonwanasuccess@gmail.com>
2019-07-10 03:17:56 -0700

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  // Team A
+ teamA:number=0;
+  addA(num:number) {
+    this.teamA=this.teamA+num;
+    
+}
+subA(num:number) {
+  this.teamA=this.teamA-num;
+  
+}
 
+//Team B
+teamB:number=0;
+  addB(num:number) {
+    this.teamB=this.teamB+num;
+    
+}
+subB(num:number) {
+  this.teamB=this.teamB-num;
+  
+}
+
+//reset button
+reset(){
+  this.teamA=0;
+  this.teamB=0;
+}
   constructor() {}
 
 }
